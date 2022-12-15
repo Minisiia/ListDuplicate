bc805049e3c25686d966f31835442c4e00a59a34
Minisiia <ar_tik@yahoo.com>
2022-12-15 22:27:15 +0200

    Java Advanced 2. Collections

--- src/list/Main.java
+++ src/list/Main.java
@@ -9,6 +9,8 @@ public class Main {
         stringList.add("Апельсини");
         stringList.add("Мандарини");
         stringList.add("І мигдаль");
+        stringList.add("Не ростуть на ялинці");
+        stringList.add("Нажаль");
         System.out.println("Початковий список:");
         for (String temp : stringList) {
             System.out.println(temp);
