75617a1a190c095042f459183f71a0718e86fd52
Minisiia <ar_tik@yahoo.com>
2023-04-22 16:12:27 +0300

    Java Advanced 2. Collections

---
+++ src/list/ListHandler.java
@@ -0,0 +1,64 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class ListHandler {
+
+    public static void printList(List<String> stringList) {
+        for (String temp : stringList) {
+            System.out.print(temp + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public static List<String> enterList() {
+        Scanner scanner = new Scanner(System.in);
+        List<String> stringList = new ArrayList<>();
+
+        System.out.println("Enter 5 words with enter:");
+        for (int i = 0; i < 5; i++) {
+            stringList.add(scanner.nextLine());
+        }
+        return stringList;
+    }
+
+
+    public static List<String> doubleValues(List<String> list) {
+        List<String> dubbedList = new ArrayList<>(list);
+        for (int i = 0; i < list.size(); i++) {
+            dubbedList.add((i + (i + 1)), list.get(i));
+        }
+        return dubbedList;
+
+    }
+
+    public static List<String> doubleValuesWithLambda(List<String> list) {
+        List<String> dubbedList = new ArrayList<>(list);
+        IntStream.iterate(0, i -> i + 2)
+                .limit(dubbedList.size())
+                .forEach(i -> dubbedList.add(i, dubbedList.get(i)));
+        return dubbedList;
+    }
+
+    public static List<String> doubleValuesWithListIterator(List<String> list) {
+        List<String> dubbedList = new ArrayList<>(list);
+        ListIterator<String> iterator = dubbedList.listIterator();
+        while (iterator.hasNext()) {
+            iterator.add(iterator.next());
+        }
+        return dubbedList;
+    }
+
+    public static List<String> doubleValuesWithLambdaFlatmap(List<String> list) {
+        return list.stream()
+                .flatMap(s -> Stream.of(s, s))
+                .collect(Collectors.toList());
+    }
+}
--- src/list/Main.java
+++ src/list/Main.java
@@ -3,23 +3,30 @@ package list;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *Використовуючи колекцію, підвійте слово:
+ * 1. Введіть із клавіатури 5 слів до списку рядків.
+ * 2. Метод doubleValues повинен підвоювати слова за принципом a, b, c -> a, a, b, b, c, c.
+ * 3. Використовуючи цикл for виведіть результат на екран, кожне значення нового рядка.
+ */
+
 public class Main {
     public static void main(String[] args) {
-        List<String> stringList = new ArrayList<>();
-        stringList.add("Апельсини");
-        stringList.add("Мандарини");
-        stringList.add("І мигдаль");
-        stringList.add("Не ростуть на ялинці");
-        stringList.add("Нажаль");
-        System.out.println("Початковий список:");
-        for (String temp : stringList) {
-            System.out.println(temp);
-        }
-        System.out.println("Продубльований список:");
-        List<String> stringListDouble = ListDuplicate.doubleValues(stringList);
-        for (String temp : stringListDouble) {
-            System.out.println(temp);
-        }
+        List<String> stringList = ListHandler.enterList();
+        System.out.println("Initial list:");
+        ListHandler.printList(stringList);
 
+        System.out.println("Duplicate list:");
+        ListHandler.printList(ListHandler.doubleValues(stringList));
+
+        System.out.println("Duplicate list with Lambda and IntStream:");
+        ListHandler.printList(ListHandler.doubleValuesWithLambda(stringList));
+
+        System.out.println("Duplicate list with ListIterator:");
+        ListHandler.printList(ListHandler.doubleValuesWithListIterator(stringList));
+
+        System.out.println("Duplicate list with Lambda and Flatmap:");
+        ListHandler.printList(ListHandler.doubleValuesWithLambdaFlatmap(stringList));
     }
+
 }
